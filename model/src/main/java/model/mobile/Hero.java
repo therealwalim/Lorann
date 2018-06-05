282b4ac6248668a807c406e957fd497530af59e7
Klimqx <33391542+soltaneben@users.noreply.github.com>
2018-06-05 15:21:31 +0100

    Update

--- model/src/main/java/model/mobile/Hero.java
+++ model/src/main/java/model/mobile/Hero.java
@@ -4,33 +4,32 @@ import contract.IElement;
 import contract.IHero;
 import contract.IView;
 import contract.MobileOrder;
+
 import model.Element;
 
 import java.awt.*;
 
-/**
- * Created by Quentin Strinati on 14/06/2016.
- */
+
 
 /** Class creating the hero extending the Mobile class and using the IHero interface */
-public class Hero extends Mobile implements IHero {
-    /**
-     * Method which constructs the Hero object
-     * @param pos variable indicating the position of the hero
-     */
+public class Hero extends Mobile implements IHero,Runnable{
+   
+int nbr = 1 ;
     public Hero(Point pos) {
+
+    
+    
         super("lorann_b.png", true, pos);
+        Thread chronoAiles  = new Thread(this);
+chronoAiles.start();
+     
+
     }
 
-    /**
-     * Method that makes the hero moves, and loads the right sprite in our tileMap
-     * @param order contains the order to give such as moving left or right
-     * @param tileMap contains the map loaded in memory which is updated each time
-     * @param view contains the view interface
-     */
-    public void move(MobileOrder order, IElement[][] tileMap, IView view)
+        public void move(MobileOrder order, IElement[][] tileMap, IView view)
     {
         switch (order) {
+            
             case Left:
                 if(this.direction == MobileOrder.Left &&
                         this.pos.y > 0 &&
@@ -39,13 +38,16 @@ public class Hero extends Mobile implements IHero {
                     this.pos.setLocation(
                             this.pos.getX(),
                             this.pos.getY() - 1);
+                   
                     this.image = Element.loadSprite("lorann_l.png");
 
                 } else { if(tileMap[this.pos.x][this.pos.y - 1].getPermeability()) {
-                	this.pos.setLocation(
+                this.pos.setLocation(
                             this.pos.getX(),
                             this.pos.getY() - 1);
                     this.image = Element.loadSprite("lorann_l.png");
+                  
+                   
                 }}
                 
                 break;
@@ -62,6 +64,7 @@ public class Hero extends Mobile implements IHero {
                             this.pos.getX(),
                             this.pos.getY() + 1);
                     this.image = Element.loadSprite("lorann_r.png");
+                  
                     }
                 }
                 break;
@@ -73,11 +76,12 @@ public class Hero extends Mobile implements IHero {
                             this.pos.getX() - 1,
                             this.pos.getY());
                 } else {
-                	if(tileMap[this.pos.x - 1][this.pos.y].getPermeability()){
+                if(tileMap[this.pos.x - 1][this.pos.y].getPermeability()){
                         this.pos.setLocation(
                                 this.pos.getX() - 1,
                                 this.pos.getY());
-                    this.image = Element.loadSprite("lorann_u.png");
+                    this.image = Element.loadSprite("lorann_u.png"); 
+                
                 }}
                 break;
             case Down:
@@ -88,15 +92,120 @@ public class Hero extends Mobile implements IHero {
                             this.pos.getX() + 1,
                             this.pos.getY());
                 } else {
-                	if(tileMap[this.pos.x + 1][this.pos.y].getPermeability()) {
+                if(tileMap[this.pos.x + 1][this.pos.y].getPermeability()) {
                     this.image = Element.loadSprite("lorann_b.png");
+                
                     this.pos.setLocation(
                             this.pos.getX() + 1,
                             this.pos.getY());
-                	}
+                }
                 }
                 break;
+               
+               
+        
         }
+        
         this.direction = order;
+      
+
+        
+        
+        switch (order) {
+        
+        case Left:
+            if(this.direction == MobileOrder.Left )
+            {
+                
+               
+                this.image = Element.loadSprite("lorann_l.png");
+
+            
+               
+            }
+            
+            break;
+        case Right:
+            if(this.direction == MobileOrder.Right)
+            {
+            this.image = Element.loadSprite("lorann_r.png");
+            } 
+               
+              
+            break;
+        case Up:
+            if(this.direction == MobileOrder.Up ) {
+            this.image = Element.loadSprite("lorann_u.png"); 
+            } 
+               
+            
+            
+            break;
+        case Down:
+            if(this.direction == MobileOrder.Down ) {
+                
+            
+                this.image = Element.loadSprite("lorann_b.png");
+            }
+            break;
+           
+           
+    
     }
+    
+    this.direction = order;
+        
+    }
+
+        
+  
+
+
+public void run() {
+while(true) {
+if(nbr==1 ) {
+     this.image =    Element.loadSprite("lorann_b.png");
+       }
+       if(nbr==2) {
+       this.image =   Element.loadSprite("lorann_bl.png");
+       }
+       if(nbr==3) {
+       this.image = Element.loadSprite("lorann_l.png");
+       }
+        if(nbr==4) {
+        this.image =  Element.loadSprite("lorann_ul.png");
+        }
+        if(nbr==5) {
+        this.image =      Element.loadSprite("lorann_u.png");
+       }
+       if(nbr==6) {
+       this.image =   
+Element.loadSprite("lorann_ur.png");
+       }
+       if(nbr==7) {
+       this.image =  Element.loadSprite("lorann_r.png");
+       }
+       if(nbr==8) {
+       this.image =
+Element.loadSprite("lorann_br.png");
+       }
+      if(nbr==8) {
+     nbr = 1;
+       }
+     nbr++;
+     
+     try {
+Thread.sleep(200);
+} catch (InterruptedException e) {
+// TODO Auto-generated catch block
+e.printStackTrace();
 }
+}
+}
+
+        
+    
+}
+     
+
+
